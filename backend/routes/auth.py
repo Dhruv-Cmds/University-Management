111a215c42bc815e4c9c3e219bcf9394aa1e23fc
Dhruv-Cmds <pd1900565@gmail.com>
2026-05-09 17:22:12 +0530

    refactor auth routes and add request limiter

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,80 +1,71 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.schemas import AdminLogin, AdminCreate, AdminResponse
+from backend.models import AdminRole
 
 from backend.services import admin_service, login_admin
-
 from backend.dependencies import get_db, require_role
 
-from backend.models import AdminRole
+from backend.core import limiter
 
 
 router = APIRouter(prefix="/admin", tags=["Admin"])
 
 @router.post("/signup", response_model=AdminResponse)
-def create_admin(admin_data: AdminCreate, db: Session = Depends(get_db)):
-    try:
-        return admin_service.create_admin(db, admin_data)
-    
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+@limiter.limit("3/second")
+async def create_admin(
+    request: Request, 
+    admin_data: AdminCreate,
+    db: AsyncSession = Depends(get_db)):
+
+    return admin_service.create_admin(db, admin_data)
 
 
 @router.post("/login")
-def login(admin_data: AdminLogin, db: Session = Depends(get_db)):
+@limiter.limit("5/minute")
+async def login(
+    request: Request, 
+    admin_data: AdminLogin,
+    db: AsyncSession = Depends(get_db)):
 
-    try:
-        return login_admin(
-            db,
-            admin_data.email,
-            admin_data.password
-        )
-
-    except ValueError as e:
-        
-        raise HTTPException(status_code=401, detail=str(e))
+    return login_admin(
+        db,
+        admin_data.email,
+        admin_data.password
+    )
 
 
 @router.get("/", response_model= list[AdminResponse])
-def get_all_admin(
-                    db:Session = Depends(get_db),
-                    current_user = Depends(require_role([AdminRole.admin]))
-                ):
+@limiter.limit("3/second")
+async def get_all_admin(
+        request: Request,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin]))
+    ):
 
-    try:
 
-        return admin_service.get_all_admin(db)
-    
-    except ValueError as e:
+    return admin_service.get_all_admin(db)
 
-        raise HTTPException(status_code=400, detail=str(e))
     
 @router.get("/{admin_id}", response_model= AdminResponse)
-def get_admin_by_id(
-                    admin_id: int,
-                    db:Session = Depends(get_db),
-                    current_user = Depends(require_role([AdminRole.admin]))
-                    ):
+@limiter.limit("3/second")
+async def get_admin_by_id(
+        request: Request, 
+        admin_id: int,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin]))
+    ):
 
-    try:
-
-        return admin_service.get_admin_by_id(db, admin_id)
-    
-    except ValueError as e:
-
-        raise HTTPException (status_code=404, detail= str(e))
+    return admin_service.get_admin_by_id(db, admin_id)
     
 @router.delete("/{admin_id}", response_model= AdminResponse)
-def delete_admin_by_id(
-                course_id: int,
-                db:Session = Depends(get_db),
-                current_user = Depends(require_role([AdminRole.admin]))
-                ):
-    try:
+@limiter.limit("2/second")
+async def delete_admin_by_id(
+        request: Request, 
+        course_id: int,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin]))
+    ):
 
-        return admin_service.delete_admin_by_id(db, course_id)
-    
-    except ValueError as e:
-
-        raise HTTPException (status_code=404, detail= str(e))
+    return admin_service.delete_admin_by_id(db, course_id)
