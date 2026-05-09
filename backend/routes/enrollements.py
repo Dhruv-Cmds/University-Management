1c742081988f9bd0965895e1b81a104d1c67c401
Dhruv-Cmds <pd1900565@gmail.com>
2026-05-09 17:32:32 +0530

    refactor   routes and add request limiter

--- backend/routes/enrollements.py
+++ backend/routes/enrollements.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.services import enrollment_service
 from backend.schemas import EnrollmentCreate, EnrollmentResponse
@@ -7,64 +7,53 @@ from backend.schemas import EnrollmentCreate, EnrollmentResponse
 from backend.dependencies import get_db, require_role
 from backend.models import AdminRole
 
+from backend.core import limiter
+
 router = APIRouter(prefix="/enrollment", tags=["Enrollments"])
 
 # only what’s in EnrollmentResponse is returned.
 @router.post("/", response_model = EnrollmentResponse)
-def create_enrollment(
-                course: EnrollmentCreate,
-                db:Session = Depends(get_db),
-                current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
-            ):
+@limiter.limit("3/second")
+async def create_enrollment(
+        request: Request,
+        course: EnrollmentCreate,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
+    ):
 
-    try:
-        # create_course not belongs to this function it's belongs to sevice folder
-        return enrollment_service.create_enrollment(db, course)
-    
-    except ValueError as e:
-
-        raise HTTPException(status_code=400, detail=str(e))
+    # create_course not belongs to this function it's belongs to sevice folder
+    return await enrollment_service.create_enrollment(db, course)
     
 @router.get("/", response_model= list[EnrollmentResponse])
-def get_all_enrollments(
-                        db:Session = Depends(get_db),
-                        current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
-                    ):
+@limiter.limit("3/second")
+async def get_all_enrollments(
+        request: Request,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
+    ):
 
-    try:
 
-        return enrollment_service.get_all_enrollments(db)
-    
-    except ValueError as e:
-
-        raise HTTPException(status_code=400, detail=str(e))
+    return await enrollment_service.get_all_enrollments(db)
     
 @router.get("/{enrollment_id}", response_model= EnrollmentResponse)
-def get_enrollment_by_id(
-                    enrollment_id: int,
-                    db:Session = Depends(get_db),
-                    current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
-                ):
+@limiter.limit("3/second")
+async def get_enrollment_by_id(
+        request: Request,
+        enrollment_id: int,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
+    ):
 
-    try:
-
-        return enrollment_service.get_enrollment_by_id(db, enrollment_id)
+    return await enrollment_service.get_enrollment_by_id(db, enrollment_id)
     
-    except ValueError as e:
-
-        raise HTTPException (status_code=404, detail= str(e))
     
 @router.delete("/{enrollment_id}", response_model= EnrollmentResponse)
-def delete_enrollment(
-                enrollment_id: int,
-                db:Session = Depends(get_db),
-                current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
-            ):
-     
-    try:
+@limiter.limit("2/second")
+async def delete_enrollment(
+        request: Request,
+        enrollment_id: int,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
+    ):
 
-        return enrollment_service.delete_enrollment(db, enrollment_id)
-    
-    except ValueError as e:
-
-        raise HTTPException (status_code=404, detail= str(e))
+    return await enrollment_service.delete_enrollment(db, enrollment_id)
