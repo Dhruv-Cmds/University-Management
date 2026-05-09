b2c0dfb20e9558bbd92af7786b4458f4228a8ab1
Dhruv-Cmds <pd1900565@gmail.com>
2026-05-09 17:47:50 +0530

    refactor faculty routes and add request limiter

--- backend/routes/faculty.py
+++ backend/routes/faculty.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from backend.services import faculty_service
 from backend.schemas import FacultyCreate, FacultyResponse
@@ -7,64 +7,52 @@ from backend.schemas import FacultyCreate, FacultyResponse
 from backend.dependencies import get_db, require_role
 from backend.models import AdminRole
 
+from backend.core import limiter
+
 router = APIRouter(prefix="/faculties", tags=["Faculties"])
 
 # only what’s in FacultyResponse is returned.
 @router.post("/", response_model = FacultyResponse)
-def create_faculty(
-                faculty: FacultyCreate,
-                db:Session = Depends(get_db),
-                current_user = Depends(require_role([AdminRole.admin]))
-            ):
-
-    try:
-        # create_course not belongs to this function it's belongs to sevice folder
-        return faculty_service.create_faculty(db, faculty)
+@limiter.limit("3/second")
+async def create_faculty(
+        request: Request,
+        faculty: FacultyCreate,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin]))
+    ):
     
-    except ValueError as e:
-
-        raise HTTPException(status_code=400, detail=str(e))
+    # create_course not belongs to this function it's belongs to sevice folder
+    return await faculty_service.create_faculty(db, faculty)
     
 @router.get("/", response_model= list[FacultyResponse])
-def get_all_faculties(
-                    db:Session = Depends(get_db),
-                    current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
-                ):
+@limiter.limit("3/second")
+async def get_all_faculties(
+        request: Request,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
+    ):
 
-    try:
-
-        return faculty_service.get_all_faculties(db)
-    
-    except ValueError as e:
-
-        raise HTTPException(status_code=400, detail=str(e))
+    return await faculty_service.get_all_faculties(db)
     
 @router.get("/{faculty_id}", response_model= FacultyResponse)
-def get_faculty_by_id(
-                    faculty_id: int,
-                    db:Session = Depends(get_db),
-                    current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
-                ):
+@limiter.limit("3/second")
+async def get_faculty_by_id(
+        request: Request,
+        faculty_id: int,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
+    ):
 
-    try:
-
-        return faculty_service.get_faculty_by_id(db, faculty_id)
+    return await faculty_service.get_faculty_by_id(db, faculty_id)
     
-    except ValueError as e:
-
-        raise HTTPException (status_code=404, detail= str(e))
     
 @router.delete("/{faculty_id}", response_model= FacultyResponse)
-def delete_faculty(
-                faculty_id: int,
-                db:Session = Depends(get_db),
-                current_user = Depends(require_role([AdminRole.admin]))
-            ):
+@limiter.limit("2/second")
+async def delete_faculty(
+        request: Request,
+        faculty_id: int,
+        db:AsyncSession = Depends(get_db),
+        current_user = Depends(require_role([AdminRole.admin]))
+    ):
      
-    try:
-
-        return faculty_service.delete_faculty(db, faculty_id)
-    
-    except ValueError as e:
-
-        raise HTTPException (status_code=404, detail= str(e))
+    return await faculty_service.delete_faculty(db, faculty_id)  
