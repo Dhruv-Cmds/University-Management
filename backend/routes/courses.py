9718f5d2d6f5ee53afbeddf87448f599840ac33f
Dhruv-Cmds <pd1900565@gmail.com>
2026-05-10 10:07:59 +0530

    add summaries and descriptions to course routes

--- backend/routes/courses.py
+++ backend/routes/courses.py
@@ -12,12 +12,17 @@ from backend.core import limiter
 router = APIRouter(prefix="/courses", tags=["Courses"])
 
 # only what’s in CourseResponse is returned.
-@router.post("/", response_model = CourseResponse)
+@router.post(
+    "/", 
+    response_model=CourseResponse,
+    summary="Create course",
+    description="Create a new course with the provided course details."
+)
 @limiter.limit("3/second")
 async def create_course(
         request: Request,
         course: CourseCreate,
-        db:AsyncSession = Depends(get_db),
+        db: AsyncSession = Depends(get_db),
         current_user = Depends(
             require_role([AdminRole.admin, AdminRole.faculty])
         )
@@ -27,37 +32,51 @@ async def create_course(
     return await course_service.create_course(db, course)
     
     
-@router.get("/", response_model= list[CourseResponse])
+@router.get(
+    "/", 
+    response_model=list[CourseResponse],
+    summary="Get all courses",
+    description="Retrieve a list of all available courses."
+)
 @limiter.limit("3/second")
 async def get_all_courses(
         request: Request,
-        db:AsyncSession = Depends(get_db),
+        db: AsyncSession = Depends(get_db),
         current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
 ):
 
     return await course_service.get_all_courses(db)
     
     
-@router.get("/{course_id}", response_model= CourseResponse)
+@router.get(
+    "/{course_id}", 
+    response_model=CourseResponse,
+    summary="Get course by ID",
+    description="Retrieve details of a specific course using its ID."
+)
 @limiter.limit("3/second")
 async def get_course_by_id(
         request: Request,
         course_id: int,
-        db:AsyncSession = Depends(get_db),
+        db: AsyncSession = Depends(get_db),
         current_user = Depends(require_role([AdminRole.admin, AdminRole.faculty]))
     ):
 
     return await course_service.get_course_by_id(db, course_id)
     
     
-@router.delete("/{course_id}", response_model= CourseResponse)
+@router.delete(
+    "/{course_id}", 
+    response_model=CourseResponse,
+    summary="Delete course",
+    description="Delete a specific course using the course ID."
+)
 @limiter.limit("2/second")
 async def delete_course(
         request: Request,
         course_id: int,
-        db:AsyncSession = Depends(get_db),
+        db: AsyncSession = Depends(get_db),
         current_user = Depends(require_role([AdminRole.admin]))
     ):
     
     return await course_service.delete_course(db, course_id)
-    
